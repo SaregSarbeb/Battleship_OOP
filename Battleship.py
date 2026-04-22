1eefb114a151d3a8d9b1c4fce059c63a1cb65108
Sareg Sarbeb <dominykas@jursys.net>
2026-04-22 20:26:51 +0300

    v4

--- Battleship.py
+++ Battleship.py
@@ -2,17 +2,24 @@ import random
 
 class Board:
     def __init__(self, size):
-        self.size = size
-        self.board = [['~'] * size for _ in range(size)]
+        self._size = size
+        self._board = [['~'] * size for _ in range(size)]
+
+        @property
+        def size(self):
+            return self._size
+        
+        @property
+        def board(self):
+            return self._board
 
     def display(self):
         # print column labels with correct offset for row numbers
-        label_width = len(str(self.size))
-        header = ' ' * label_width + ' ' + ' '.join(chr(ord('A') + i) for i in range(self.size))
+        label_width = len(str(self._size))
+        header = ' ' * label_width + ' ' + ' '.join(chr(ord('A') + i) for i in range(self._size))
         print(header)
-
         # print each row with its number on the left, aligned to width
-        for row_index, row in enumerate(self.board, start=1):
+        for row_index, row in enumerate(self._board, start=1):
             print(f"{row_index:>{label_width}} {' '.join(row)}")
 
     def place_ship(self, ship, coords, orien):
@@ -22,11 +29,11 @@ class Board:
         col = ord(letter) - ord('A')
         row = int(number) - 1
         if orien == 'H':
-            if col + ship.size > self.size:
+            if col + ship.size > self._size:
                 return False
             ship_cells = [(row, col + i) for i in range(ship.size)]
         else:
-            if row + ship.size > self.size:
+            if row + ship.size > self._size:
                 return False
             ship_cells = [(row + i, col) for i in range(ship.size)]
         #check adjacent and ship cells
@@ -35,13 +42,12 @@ class Board:
                 for dc in (-1, 0, 1):
                     nr = r + dr
                     nc =  c + dc
-                    if 0 <= nr < self.size and 0 <= nc < self.size:
-                        if self.board[nr][nc] != '~':
+                    if 0 <= nr < self._size and 0 <= nc < self._size:
+                        if self._board[nr][nc] != '~':
                             return False
         #place ship
         for r, c in ship_cells:
-            self.board[r][c] = 'S'
-
+            self._board[r][c] = 'S'
         return True
 
     def shoot(self, coords):
@@ -49,12 +55,12 @@ class Board:
         number = int(coords[1:])
         col = ord(letter) - ord('A')
         row = int(number) - 1
-        cell = self.board[row][col]
+        cell = self._board[row][col]
         if cell == '~':
-            self.board[row][col] = 'O'
+            self._board[row][col] = 'O'
             return 'miss'
         elif cell == 'S':
-            self.board[row][col] = 'X'
+            self._board[row][col] = 'X'
             return 'hit'
         else:
             return 'alr_shot'
@@ -64,18 +70,18 @@ class AIBoard(Board):
         super().__init__(size)
 
     def display(self):
-        # Hide ships (show ~ instead of S)
-        label_width = len(str(self.size))
-        header = ' ' * label_width + ' ' + ' '.join(chr(ord('A') + i) for i in range(self.size))
+        # Hides ships (shows ~ instead of S)
+        label_width = len(str(self._size))
+        header = ' ' * label_width + ' ' + ' '.join(chr(ord('A') + i) for i in range(self._size))
         print(header)
-        for row_index, row in enumerate(self.board, start=1):
+        for row_index, row in enumerate(self._board, start=1):
             hidden_row = ['~' if cell == 'S' else cell for cell in row]
             print(f"{row_index:>{label_width}} {' '.join(hidden_row)}")
 
     def place_ship_random(self, ship):
         while True:
-            row = random.randint(0, self.size - 1)
-            col = random.randint(0, self.size - 1)
+            row = random.randint(0, self._size - 1)
+            col = random.randint(0, self._size - 1)
             orien = random.choice(['H', 'V'])
             coords = chr(ord('A') + col) + str(row + 1)
             if self.place_ship(ship, coords, orien):
@@ -83,7 +89,13 @@ class AIBoard(Board):
 
 class Ship:
     def __init__(self, size):
-        self.size = size
+        self._size = size
+
+    @property
+    def size(self):
+        return self._size
+    
+#----GAME SETUP----
 
 s4 = Ship(5)
 s3 = Ship(4)
@@ -94,7 +106,7 @@ ships = [s4, s3, s2, s1, s0]
 player_board = Board(10)
 ai_board = AIBoard(10)
 
-for i in range(5):
+for i in range(5): #Player ship placement
     while True:
         while True:
             coords = input("Input coordinates(A1-J10):")
@@ -112,21 +124,26 @@ for i in range(5):
             else:
                 break
         if player_board.place_ship(ships[i], coords, orient) == True:
+            player_board.display()
             break
         else:
             print("Cannot place ship there - occupied, too close to another ship or out of bounds")
+
 player_board.display()
 ai_board.display()
-for i in range(5):
+
+for i in range(5): #AI ship placement(random)
     while True:
        if ai_board.place_ship_random(ships[i]):
             break
+       
 print("Shooting phase")
 player_shots_fired = 0
 player_hits = 0
 ai_shots_fired = 0
 ai_hits = 0
 total_ship_hp = sum(ship.size for ship in ships)
+
 while player_hits < total_ship_hp and ai_hits < total_ship_hp:
     print("Player turn")
     player_turn = True
